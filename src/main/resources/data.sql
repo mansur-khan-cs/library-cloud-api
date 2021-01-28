ff97c23041a5ce549903be548297fdf8ecd06d0a
Zahirul <cse.Zahirulislam@gmail.com>
2021-01-29 00:01:50 +0100

    data changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO BOOKS(id,title,author,publisher) VALUES(1,'Spektrum Deutche A2','Anne','Schubert')
 INSERT INTO BOOKS(id,title,author,publisher) VALUES(2,'Head first Java','Bert Bates and Kathey Sierra','OReilly')
-INSERT INTO BOOKS(id,title,author,publisher) VALUES(3,'Effective Java','Joshua Bloch','Sun')
+
 
